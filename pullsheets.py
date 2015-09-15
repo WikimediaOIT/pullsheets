c008b296e972ed6a9b73b6e0d7825d7770d9d4a2
Joel Krauska <jkrauska@gmail.com>
2015-09-15 10:14:22 -0700

    Updates

--- pullsheets.py
+++ pullsheets.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+from __future__ import print_function # In case running on Python 2
+
+
 # Google oauth secrice account credentials file
 # http://gspread.readthedocs.org/en/latest/oauth2.html
 # be sure to add service account email to document with View permissions
@@ -15,6 +18,9 @@ picklefile = 'pullsheets.pkl'
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
+
+from deepdiff import DeepDiff
+from pprint import pprint
 
 # Pull configuration from local file
 from pullsheets_config import *
@@ -39,10 +45,10 @@ bigdata = []
 
 # Start looking at spreadsheets
 for spreadsheet in sheetlist:
-    print 'Speadsheet:', spreadsheet
+    print('Speadsheet:', spreadsheet)
     # Look at sub worksheets
     for subsheet in sheetlist[spreadsheet]:
-        print '\tWorksheet', subsheet
+        print(' -- Worksheet', subsheet)
         worksheet = gc.open(spreadsheet).worksheet(subsheet)
 
         # Walk each row of the sheet
@@ -54,11 +60,13 @@ for spreadsheet in sheetlist:
 
             # Decode fields and store values
             for field,location in sheetlist[spreadsheet][subsheet].items():
-                output[field] = row[location]
+                output[field] = row[location].strip()
 
             # Bounds checking, ignore this row
             if output['FirstName'] == 'First Name': continue
             if len(output['FirstName']) == 0: continue
+            if len(output['LastName']) == 0: continue
+
 
             # Unicode decode (umlauts!)
             try: output['LastName'] = unicodedata.normalize(
@@ -71,22 +79,47 @@ for spreadsheet in sheetlist:
                 email_guess = "%s%s@wikimedia.org" % (
                     output['FirstName'][0].lower(), output['LastName'].lower())
             except:
-                print 'PARSE ERROR', row
+                print('PARSE ERROR', row)
             output['Email (guess)'] = email_guess
 
+            # Infer wiki user account address
+            try:
+                wiki_user = "%s%s (WMF)" % (
+                    output['FirstName'][0], output['LastName'])
+            except:
+                print('PARSE ERROR', row)
+            output['wiki username'] = wiki_user
             bigdata.append(output)
-
 
 # Compare lastdata with bigdata
 changefound=False
 for record in bigdata:
-    if record not in lastdata:
-        print '-'*80
-        print 'Change Found:'
-        print json.dumps(record, sort_keys=True, indent=4)
-        changefound=True
+    recordfound=False
+    for oldrecord in lastdata:
+        if (record['FirstName'] == oldrecord['FirstName'] and
+            record['LastName'] == oldrecord['LastName'] and
+            record['Spreadsheet'] == oldrecord['Spreadsheet']
+            ):
+            recordfound=True
+            ddiff = DeepDiff(oldrecord, record)
+
+            if len(ddiff) > 0:
+                print('-'*80)
+                print('Changed Record:')
+                print(json.dumps(record, sort_keys=True, indent=4))
+                print(json.dumps(ddiff, sort_keys=True, indent=4))
+                changefound=True
+    if not recordfound:
+            print('-'*80)
+            print('New Record:')
+            print(json.dumps(record, sort_keys=True, indent=4))
+            changefound=True
+
+
+
 
 if changefound:
-    pickle.dump( bigdata, open( picklefile, "wb" ) )
+    print('Saving changes to file.')
+    pickle.dump(bigdata, open(picklefile, "wb"))
     picklefile_ts = "%s-%s" % (picklefile, time.strftime("%Y%m%d-%H%M"))
-    pickle.dump( bigdata, open( picklefile_ts, "wb" ) )
+    pickle.dump(bigdata, open(picklefile_ts, "wb"))
